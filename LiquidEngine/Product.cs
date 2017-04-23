a7f1ee428c82125c7b79126924370fcba3ee9aab
Giancarlos Salas <giansalex@gmail.com>
2017-04-22 19:24:00 -0500

    Add project files.

---
+++ LiquidEngine/Product.cs
@@ -0,0 +1,14 @@
+﻿using DotLiquid;
+
+namespace LiquidEngine
+{
+    /// <summary>
+    /// Producto.
+    /// </summary>
+    public class Product : Drop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ LiquidEngine/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using DotLiquid;
+
+namespace LiquidEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.WriteLine("Starting Render");
+            var watch = Stopwatch.StartNew();
+            Run();
+            watch.Stop();
+            Console.WriteLine("Time elapsed : " + watch.ElapsedMilliseconds + " ms" );
+            Console.ReadKey();
+        }
+
+        static void Run()
+        {
+            var products = new[]
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    Price = 1224.54M
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Sofa 2M",
+                    Price = 123.40M
+                } 
+            };
+            //Template.RegisterSafeType(typeof(Product), new[]
+            //{
+            //    nameof(Product.Id),
+            //    nameof(Product.Name),
+            //    nameof(Product.Price)
+            //});
+            var pathSource = "./template.liquid";
+            var source = File.ReadAllText(pathSource);
+            var template = Template.Parse(source);
+
+            var result = template.Render(Hash.FromAnonymousObject(new
+            {
+                products
+            }));
+            Console.WriteLine(result);
+        }
+    }
+}
